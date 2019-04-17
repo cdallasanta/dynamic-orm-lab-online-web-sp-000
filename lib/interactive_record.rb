b4ded97653b748905560c4c08b05cbda3745a9f7
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 01:39:47 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -75,7 +75,7 @@ class InteractiveRecord
       sql = <<-SQL
         SELECT *
         FROM #{table_name}
-        WHERE #{key.to_s} = '?';
+        WHERE #{key.to_s} = ?;
       SQL
 
       DB[:conn].execute(sql, value)
