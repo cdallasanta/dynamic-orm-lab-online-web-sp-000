18af820ee997c40aceaa9d4540995e30d3fd8407
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-17 01:23:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -75,10 +75,10 @@ class InteractiveRecord
       sql = <<-SQL
         SELECT *
         FROM #{table_name}
-        WHERE grade = '10';
+        WHERE ? = '?';
       SQL
 
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql, key.to_s, value)
     end
   end
 end
